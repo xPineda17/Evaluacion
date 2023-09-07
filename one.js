8df5aa67d00ae515a4f2a891e9b82794921f95a5
xPineda17 <113036076+xPineda17@users.noreply.github.com>
2023-09-07 08:27:13 -0500

    Add files via upload

---
+++ one.js
@@ -0,0 +1,8 @@
+let sueldos = [];
+let empleados = parseInt(prompt('Ingrese la cantidad de empleados: '))
+
+for(x=1; x<=empleados; x++){
+    let empleado = prompt('Nombre empleado: ')
+    let sueldo = parseInt(prompt('Sueldo: '))
+    sueldo = sueldos.push()
+}
